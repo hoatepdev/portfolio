1e402d5348348e943ea20a3841c2d48a59be5044
hoatepdev <hoanguyentrandev@gmail.com>
2025-05-24 06:03:29 +0700

    feat: ✨ Add emoij message commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [0],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ],
+    ],
+    "header-max-length": [2, "always", 100],
+  },
+  // This pattern enforces: type(scope?): emoji subject
 };
