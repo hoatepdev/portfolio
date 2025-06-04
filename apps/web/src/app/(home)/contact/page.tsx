c03277193f82e86e3c949cb37eda1fe654f0779d
hoatepdev <hoanguyentrandev@gmail.com>
2025-06-04 17:52:55 +0700

    fix(contact): 🐛 change MapBox URL for improved accuracy

--- apps/web/src/app/(home)/contact/page.tsx
+++ apps/web/src/app/(home)/contact/page.tsx
@@ -7,7 +7,7 @@ import PageHeader from "@/components/page-header";
 import MapBox from "@/components/section/contact/map-box";
 import config from "@/config";
 
-const { title } = config;
+const { title, about } = config;
 
 /**
  * TODO: #341 still need to update with another method to avoid client side not available metadata
@@ -23,9 +23,9 @@ function Contact() {
 
   return (
     <article>
-      <PageHeader header="Hugo's Contact" />
+      <PageHeader header={`${about.preferredName}'s Contact`} />
       <section className="mb-[10px]">
-        <MapBox googleMapUrl="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3616.9398617144193!2d121.19272387485687!3d24.968160640940518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346823c1ec904dcb%3A0xcdc129d4455ce456!2sNational%20Central%20University!5e0!3m2!1sen!2stw!4v1704610088586!5m2!1sen!2stw" />
+        <MapBox googleMapUrl="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14005.365475345357!2d105.80249969012159!3d21.00981321254594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac9e29394f65%3A0x8dc13ae6c56b08da!2zMTM5IMSQLiBOZ3V54buFbiBOZ-G7jWMgVsWpLCBUcnVuZyBIb8OgLCBD4bqndSBHaeG6pXksIEjDoCBO4buZaSAwMDE3NSwgVmlldG5hbQ!5e0!3m2!1sen!2s!4v1749034254147!5m2!1sen!2s" />
         <h3 className="text-white-2 mb-[20px] text-2xl font-bold">
           Contact Form
         </h3>
